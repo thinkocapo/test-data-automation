e7961a6d201fb2f930ed3d3a2c337b09cd5fb17d
System Administrator <root@MacBook-Pro.local>
2020-03-20 20:13:16 -0700

    documented and looping through each test

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # TODO
 
+# Run tests/simulation
 Pulls up Sentry frontend in various browsers in parallel via selenium scripts.
 Test case will add items to cart and then click checkout
 
@@ -8,3 +9,16 @@ pip install - requirements.txt
 py.test -s -n 2 tests
 
 ```
+
+# Setting up cron job to trigger simulations
+
+We can trigger the travis builds on a schedule via Google Cloud Scheduler cron jobs.
+
+To register:
+```
+./create_cron_job.sh
+```
+
+Docs:
+- https://cloud.google.com/sdk/gcloud/reference/scheduler/jobs/create/
+- https://docs.travis-ci.com/user/triggering-builds/
--- tests/test_add_to_cart.py
+++ tests/test_add_to_cart.py
@@ -1,6 +1,7 @@
 import pytest
 import time
 import yaml
+import random
 
 @pytest.mark.usefixtures("driver")
 def test_add_to_cart(driver):
@@ -10,11 +11,12 @@ def test_add_to_cart(driver):
         endpoints = data_loaded['react_endpoints']
 
     for endpoint in endpoints:
-        driver.get(endpoint)
+        for i in range(random.randrange(5)):
+            driver.get(endpoint)
 
-        buy_button = driver.find_element_by_css_selector('.item button')
-        for i in range(3):
-            buy_button.click()
+            buy_button = driver.find_element_by_css_selector('.item button')
+            for i in range(3):
+                buy_button.click()
 
-        driver.find_element_by_css_selector('.sidebar button').click()
-        time.sleep(3)
+            driver.find_element_by_css_selector('.sidebar button').click()
+            time.sleep(3)
